4962223e2e0ceaffb9e4ab815a80bdefa576d2a4
Cori Fowler <fowler.cori@gmail.com>
2015-10-13 17:20:58 -0400

    all input on page using javascript and template

--- app/js/main.js
+++ app/js/main.js
@@ -2,5 +2,34 @@
 
 (function () {
 
-  console.log('It Works!');
+  var url = 'http://json-data.herokuapp.com/forms';
+
+  var promise = $.getJSON(url);
+
+  promise.then(function (responseFromServer) {
+    doSomething(responseFromServer);
+  });
+
+  var template = _.template($('#registration-form').text());
+  var templateSelect = _.template($('#language-form').text());
+
+  var doSomething = function doSomething(arrayData) {
+
+    _.each(arrayData, function (item) {
+
+      if (item.options.length > 0) {
+        var selectBlock = templateSelect(item);
+        $('.container').append(selectBlock);
+      } else if (item.type === 'text' || item.type === 'tel' || item.type === 'email' || item.type === 'textarea') {
+        var htmlBlock = template(item);
+        $('.container').append(htmlBlock);
+      };
+    });
+  };
 })();
+
+// _.each(item.options, function(option){
+//     console.log(item.options);
+//     var selectBlock = templateSelect(option.label);
+//     $('.container').append(selectBlock);
+//   });
--- src/main.js
+++ src/main.js
@@ -1,5 +1,36 @@
 (function () {
 
-  console.log('It Works!');
+  let url = 'http://json-data.herokuapp.com/forms';
+
+  let promise = $.getJSON(url);
+
+  promise.then( function(responseFromServer){
+    doSomething(responseFromServer);
+  });
+
+  let template = _.template($('#registration-form').text());
+  let templateSelect = _.template($('#language-form').text());
+
+  let doSomething = function(arrayData) {
+
+    _.each(arrayData, function(item){
+
+    if (item.options.length > 0) {
+      var selectBlock = templateSelect(item);
+      $('.container').append(selectBlock);     
+    } else if (item.type === 'text' || item.type === 'tel' || item.type === 'email' || item.type === 'textarea') {
+        var htmlBlock = template(item);
+        $('.container').append(htmlBlock);
+      };
+
+    });
+  };
+
 
 }());
+
+      // _.each(item.options, function(option){
+      //     console.log(item.options);
+      //     var selectBlock = templateSelect(option.label);
+      //     $('.container').append(selectBlock);
+      //   });
